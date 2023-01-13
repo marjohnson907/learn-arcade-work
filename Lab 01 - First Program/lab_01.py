1fdf8de3d37d25bbcefbddecac41166dc0f3b5af
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-01-12 18:44:18 -0700

    Finished lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,11 @@
-Hi
+print("""Eleanor Roosevelet said, \"If life were predictable 
+it would cease to be life, and be without flavor.\"""")
+
+print("\"Behind every great man is a woman rolling her eyes.\" Jim Carrey")
+
+print("Oscar Wilde-\nBe yourself; everyone else is taken.")
+
+print('''Robert Frost said, \"In three words 
+I can sum up everything I've learned about life: 
+it goes on.''')
+
--- Testing/test.py
+++
@@ -1,6 +0,0 @@
-print("It was a dark and stormy night.")
-print("Suddenly a shot rang out!")
-
-print("""You can print
-on multiple lines
-using triple quotes.""")
