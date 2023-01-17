dbcf6f86043859c4deeab84cdd687959d3c11c32
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-01-16 18:13:37 -0700

    Drew two trees

--- Testing/drawing_samples.py
+++ Testing/drawing_samples.py
@@ -21,7 +21,23 @@ arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 # Get ready to draw
 arcade.start_render()
 
-# (The drawing code will go here.)
+# Draw a rectangle.
+# Left of 0, right of 599.
+# Top of 300, bottom of 0.
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+
+# Tree trunk
+# Center of 100, 320
+# Width of 20
+# Height of 60
+arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
+
+# Tree top
+arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.DARK_GREEN)
+
+# Another tree, with a trunk and an ellipse for a top.
+arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.DARK_GREEN)
 
 # Finish drawing
 arcade.finish_render()
