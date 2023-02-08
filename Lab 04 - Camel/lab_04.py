b758ae4d61834766e6f61378956ce2121d4d14af
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-02-07 18:58:44 -0700

    Continuing Lab 4, up to step 14

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,15 +1,17 @@
 import arcade
 
+miles_traveled = 0
+thirst = 0
+camel_tired = 0
+canteen_drinks = 3
+natives_distance = -20
+
 
 def main():
     print("Welcome to Camel!")
     print("""You have stolen a camel to make your way across the great Mobi desert.
 The natives want their camel back and are chasing you down! 
 Survive your desert trek and out run the natives.""")
-
-    miles_traveled = 0
-    thirst = 0
-    camel_tired = 0
 
     done = False
     while not done:
@@ -23,6 +25,19 @@ Q. Quit.""")
         user_choice = input("What is your choice? ")
         if user_choice.upper() == "Q":
             print("You have quit.")
+            done = True
+        elif user_choice.upper() == "D":
+            camel_tired = 0
+            print("Your camel is happy.")
+            natives_distance = natives_distance + 13
+            print("The natives are ", miles_traveled - natives_distance, " miles behind you.")
+        elif user_choice.upper() == "C":
+            miles_traveled = miles_traveled + 12
+            print(miles_traveled)
+            thirst = thirst + 1
+            camel_tired = camel_tired + 2
+            natives_distance = natives_distance + 13
+            print("The natives are ",miles_traveled - natives_distance, "miles behind you.")
             done = True
 
 
