da9e41f534214cb3af018fd0a789d638e522d534
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-02-18 13:52:44 -0700

    Starting Lab 6

---
+++ Lab 06 - Text Adventure/lab_06.py
@@ -0,0 +1,19 @@
+class Room:
+    """
+    This is a class that describes the room.
+    """
+    def __init__(self):
+        """Sets up the variables in the object."""
+        self.description = ""
+        self.north = ""
+        self.south = ""
+        self.east = ""
+        self.west = ""
+
+
+def main():
+    room_list = []
+    room = Room()
+
+
+main()
