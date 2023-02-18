335fee4ebb48de05eb4901a9bd1f4f77ed017a25
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-02-18 14:11:58 -0700

    Starting Lab 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -12,8 +12,15 @@ class Room:
 
 
 def main():
-    room_list = []
+    room_list = [0, 1, 2, 3, 4, 5, 6]
     room = Room()
+
+    room.description = "You are in a stone hall with a circular marble table in the center." \
+                       "Tapestries hang on the walls and there is a woven rug on the floor."
+    room.north = 4
+    room.south = None
+    room.east = 2
+    room.west = 0
 
 
 main()
