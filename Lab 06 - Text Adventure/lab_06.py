af7a643aa940e42183f3bfec718e3a068e424234
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-02-21 17:26:12 -0700

    Lab 6, step 7

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -13,14 +13,59 @@ class Room:
 
 def main():
     room_list = [0, 1, 2, 3, 4, 5, 6]
-    room = Room()
 
-    room.description = "You are in a stone hall with a circular marble table in the center." \
-                       "Tapestries hang on the walls and there is a woven rug on the floor."
-    room.north = 4
-    room.south = None
-    room.east = 2
-    room.west = 0
+    room1 = Room()
+    room1.description = "You are in a stone hall. There are doors to the east and west."
+    room1.north = None
+    room1.south = None
+    room1.east = 2
+    room1.west = 0
+
+    room2 = Room()
+    room2.description = "You are in a room. There are doors to the north and west."
+    room2.north = 5
+    room2.south = None
+    room2.east = None
+    room2.west = 1
+
+    room0 = Room()
+    room0.description = "You are in a room. There are doors to the north and east."
+    room0.north = 3
+    room0.south = None
+    room0.east = 1
+    room0.west = None
+
+    room3 = Room()
+    room3.description = "You are in a room. There are doors to the south and east."
+    room3.north = None
+    room3.south = 0
+    room3.east = 4
+    room3.west = None
+
+    room4 = Room()
+    room4.description = "You are in a hall. There are doors to the east, west, north, and south."
+    room4.north = 6
+    room4.south = 1
+    room4.east = 5
+    room4.west = 3
+
+    room5 = Room()
+    room5.description = "You are in a room. There are doors to the west and south."
+    room5.north = None
+    room5.south = 2
+    room5.east = None
+    room5.west = 4
+
+    room6 = Room()
+    room6.description = "You are on a balcony. There is a door to the south."
+    room6.north = None
+    room6.south = 4
+    room6.east = None
+    room6.west = None
+
+
+
+
 
 
 main()
