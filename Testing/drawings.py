d968e270ca35501589efb57c7f4f2db1623ce820
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-03-21 18:49:51 -0600

    Drawing astronaut

--- Testing/drawings.py
+++ Testing/drawings.py
@@ -5,12 +5,14 @@ SCREEN_HEIGHT = 500
 
 
 def draw_astronaut(x, y):
+    arcade.draw_rectangle_filled(x, y - 15, 30, 30, arcade.color.ANTIQUE_WHITE)
     arcade.draw_circle_outline(x, y, 12, arcade.color.WHITE_SMOKE, 3)
     arcade.draw_circle_filled(x, y, 10, arcade.color.GRAY)
     arcade.draw_arc_filled(x, y - 30, 20, 40, arcade.color.WHITE_SMOKE, 0, 180)
     arcade.draw_lrtb_rectangle_filled(x - 10, x - 1, y - 30, y - 40, arcade.color.WHITE_SMOKE)
     arcade.draw_lrtb_rectangle_filled(x + 1, x + 10, y - 30, y - 40, arcade.color.WHITE_SMOKE)
     arcade.draw_arc_filled(x - 15, y - 30, 10, 40, arcade.color.WHITE_SMOKE, 0, 180, 330)
+    arcade.draw_arc_filled(x - 15, y - 30, 10, 20, arcade.color.GRAY, 0, 180, 150)
     arcade.draw_arc_filled(x + 15, y - 30, 10, 40, arcade.color.WHITE_SMOKE, 0, 180, 30)
 
 
--- Testing/test.py
+++ Testing/test.py
@@ -1,32 +1,34 @@
+""" Sprite Sample Program """
+
+import random
 import arcade
 
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 500
+# --- Constants ---
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.2
+COIN_COUNT = 50
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 
-def draw_astronaut(x, y):
-    arcade.draw_circle_outline(x, y, 12, arcade.color.WHITE_SMOKE, 3)
-    arcade.draw_circle_filled(x, y, 10, arcade.color.GRAY)
-    arcade.draw_arc_filled(x, y - 30, 20, 40, arcade.color.WHITE_SMOKE, 0, 180)
-    arcade.draw_lrtb_rectangle_filled(x - 10, x - 1, y - 30, y - 40, arcade.color.WHITE_SMOKE)
-    arcade.draw_lrtb_rectangle_filled(x + 1, x + 10, y - 30, y - 40, arcade.color.WHITE_SMOKE)
-    arcade.draw_arc_filled(x - 15, y - 30, 10, 40, arcade.color.WHITE_SMOKE, 0, 180, 330)
-    arcade.draw_arc_filled(x + 15, y - 30, 10, 40, arcade.color.WHITE_SMOKE, 0, 180, 30)
+class MyGame(arcade.Window):
+    """ Our custom Window Class"""
 
+    def __init__(self):
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
-def on_draw(delta_time):
-    arcade.start_render()
-
-    draw_astronaut(250, 250)
+    def on_draw(self):
+        arcade.start_render()
 
 
 def main():
-    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
-    arcade.set_background_color(arcade.color.BLACK)
-
-    arcade.schedule(on_draw, 1/60)
-
+    """ Main method """
+    window = MyGame()
     arcade.run()
 
 
-main()
+if __name__ == "__main__":
+    main()
