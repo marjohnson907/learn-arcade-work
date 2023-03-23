24edb2cf47aabb5cdc74e907ff22fb8034aa9444
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-03-23 17:42:57 -0600

    building lab 8

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -1,9 +1,9 @@
+
 import random
 import arcade
-import math
 
-SPRITE_SCALING = 0.25
-BLOB_COUNT = 100
+SPRITE_SCALING = 0.5
+BLOB_COUNT = 50
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 700
@@ -12,4 +12,38 @@ SCREEN_HEIGHT = 700
 class MyGame(arcade.Window):
 
     def __init__(self):
-        super.__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 8")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 8")
+
+        self.player_list = None
+        self.blob_list = None
+
+        self.player_sprite = None
+        self.score = 0
+
+        self.set_mouse_visible(False)
+
+        arcade.set_background_color(arcade.color.EERIE_BLACK)
+
+    def setup(self):
+
+        self.player_list = arcade.SpriteList()
+        self.blob_list = arcade.SpriteList()
+
+        self.score = 0
+
+        self.player_sprite = arcade.Sprite("alienBlue_front.png", SPRITE_SCALING)
+        self.player_sprite.center_x = 350
+        self.player_sprite.center_y = 350
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        arcade.start_render()
+
+
+def main():
+    window = MyGame
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
