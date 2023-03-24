d3e3fe786baa6a3d0c9bdca19672a9a2042173f3
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-03-23 19:08:28 -0600

    working on lab 8

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -2,11 +2,11 @@
 import random
 import arcade
 
-SPRITE_SCALING = 0.5
-BLOB_COUNT = 50
+SPRITE_SCALING = 0.25
+STAR_COUNT = 50
 
-SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 
 
 class MyGame(arcade.Window):
@@ -15,7 +15,7 @@ class MyGame(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 8")
 
         self.player_list = None
-        self.blob_list = None
+        self.star_list = None
 
         self.player_sprite = None
         self.score = 0
@@ -27,7 +27,7 @@ class MyGame(arcade.Window):
     def setup(self):
 
         self.player_list = arcade.SpriteList()
-        self.blob_list = arcade.SpriteList()
+        self.star_list = arcade.SpriteList()
 
         self.score = 0
 
@@ -36,12 +36,46 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 350
         self.player_list.append(self.player_sprite)
 
+        for i in range(STAR_COUNT):
+
+            # Blob image from kenney.nl
+            star = arcade.Sprite("star.png", SPRITE_SCALING)
+
+            star.center_x = random.randrange(SCREEN_WIDTH)
+            star.center_y = random.randrange(SCREEN_HEIGHT)
+
+            self.star_list.append(star)
+
     def on_draw(self):
         arcade.start_render()
 
+        self.star_list.draw()
+        self.player_list.draw()
+
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+
+        # Move the center of the player sprite to match the mouse x, y
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def update(self, delta_time):
+
+        self.star_list.update()
+
+        star_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                              self.star_list)
+
+        for star in star_hit_list:
+            star.remove_from_sprite_lists()
+            self.score += 1
+
 
 def main():
-    window = MyGame
+    window = MyGame()
+    window.setup()
     arcade.run()
 
 
