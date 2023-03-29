bcbf3b4a80a02fea30fbdc401ef6db7bdab0922f
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-03-28 18:05:09 -0600

    created window & player

--- Lab 09 - Sprites and Walls/lab_09.py
+++ Lab 09 - Sprites and Walls/lab_09.py
@@ -15,11 +15,30 @@ class MyGame(arcade.Window):
 
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+        self.player_list = None
+        self.wall_list = None
+
+        self.player_sprite = None
+
+        self.physics_engine = None
+
     def setup(self):
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(arcade.color.BLACK)
+
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+
+        # self.score = 0
+        # Image from https://kenney.nl
+        self.player_sprite = arcade.Sprite("player_01.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         arcade.start_render()
+        self.wall_list.draw()
+        self.player_list.draw()
 
 
 def main():
