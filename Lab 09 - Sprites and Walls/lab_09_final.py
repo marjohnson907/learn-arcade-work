f7ad98afc68bbbb1b013295cbf3f972a47980831
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-03-31 18:47:05 -0600

    got everything working

--- Lab 09 - Sprites and Walls/lab_09_final.py
+++ Lab 09 - Sprites and Walls/lab_09_final.py
@@ -2,7 +2,7 @@ import arcade
 
 SPRITE_SCALING = 0.5
 
-SCREEN_WIDTH = 700
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 500
 
 MOVEMENT_SPEED = 5
@@ -21,6 +21,7 @@ class MyGame(arcade.Window):
         # Player
         self.player_sprite = None
 
+        # Physics engine
         self.physics_engine = None
 
     def setup(self):
@@ -39,10 +40,65 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
+        # Walls, image from https://kenney.nl
+        wall = arcade.Sprite("tileYellow_02.png", SPRITE_SCALING)
+        wall.center_x = 300
+        wall.center_y = 200
+        self.wall_list.append(wall)
+
+        wall = arcade.Sprite("tileYellow_02.png", SPRITE_SCALING)
+        wall.center_x = 550
+        wall.center_y = 200
+        self.wall_list.append(wall)
+
+        wall = arcade.Sprite("tileYellow_02.png", SPRITE_SCALING)
+        wall.center_x = 800
+        wall.center_y = 200
+        self.wall_list.append(wall)
+
+        # Image from https://kenney.nl
+        for x in range(100, 1000, 32):
+            wall = arcade.Sprite("crate_01.png", SPRITE_SCALING)
+            wall.center_x = x
+            wall.center_y = 100
+            self.wall_list.append(wall)
+
+        for x in range(100, 1000, 32):
+            wall = arcade.Sprite("crate_01.png", SPRITE_SCALING)
+            wall.center_x = x
+            wall.center_y = 300
+            self.wall_list.append(wall)
+
+        # Physics engine
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+
     def on_draw(self):
         arcade.start_render()
         self.wall_list.draw()
         self.player_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+
+    def update(self, delta_time):
+        self.physics_engine.update()
 
 
 def main():
