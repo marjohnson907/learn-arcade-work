754531460887c06e17dfada2b491991d53ca573a
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-04-17 19:53:10 -0600

    Added coins/stars/enemy to room 3, bee isn't following sprite

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -4,9 +4,10 @@ import random
 SPRITE_SCALING = 0.5
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
-NUMBER_OF_COINS = 10
-NUMBER_OF_STARS = 5
+NUMBER_OF_COINS = 1
+NUMBER_OF_STARS = 1
 MOVEMENT_SPEED = 5
+ENEMY_NUMBER = 1
 
 
 class Room:
@@ -16,6 +17,22 @@ class Room:
         self.wall_list = None
         self.coin_list = None
         self.star_list = None
+        self.enemy_list = None
+
+
+class Enemy(arcade.Sprite):
+
+    def follow_sprite(self, enemy):
+
+        if self.center_y < enemy.center_y:
+            self.center_y += min(MOVEMENT_SPEED, enemy.center_y - self.center_y)
+        elif self.center_y > enemy.center_y:
+            self.center_y -= min(MOVEMENT_SPEED, self.center_y - enemy.center_y)
+
+        if self.center_x < enemy.center_x:
+            self.center_x += min(MOVEMENT_SPEED, enemy.center_x - self.center_x)
+        elif self.center_x > enemy.center_x:
+            self.center_x -= min(MOVEMENT_SPEED, self.center_x - enemy.center_x)
 
 
 def setup_room_1():
@@ -26,6 +43,7 @@ def setup_room_1():
     room.wall_list = arcade.SpriteList()
     room.coin_list = arcade.SpriteList()
     room.star_list = arcade.SpriteList()
+    room.enemy_list = arcade.SpriteList()
 
     # Walls, Room 1, image from https://kenney.nl
     for y in (0, SCREEN_HEIGHT - 16):
@@ -92,6 +110,7 @@ def setup_room_2():
     room.wall_list = arcade.SpriteList()
     room.coin_list = arcade.SpriteList()
     room.star_list = arcade.SpriteList()
+    room.enemy_list = arcade.SpriteList()
 
     # Walls, Room 2, image from https://kenney.nl
     for y in (0, SCREEN_HEIGHT - 16):
@@ -127,7 +146,7 @@ def setup_room_2():
 
     # Coins, image from https://kenney.nl
     for i in range(NUMBER_OF_COINS):
-        coin = arcade.Sprite("coinSilver.png", SPRITE_SCALING / 2)
+        coin = arcade.Sprite("coinSilver.png", SPRITE_SCALING/2)
 
         # Boolean variable coin placement
         coin_placed_successfully = False
@@ -149,7 +168,7 @@ def setup_room_2():
 
         # Stars, image from https://kenney.nl
         for i in range(NUMBER_OF_STARS):
-            star = arcade.Sprite("star.png", SPRITE_SCALING / 2)
+            star = arcade.Sprite("star.png", SPRITE_SCALING/2)
 
             # Boolean variable star placement
             star_placed_successfully = False
@@ -178,6 +197,9 @@ def setup_room_3():
 
     # Sprite lists
     room.wall_list = arcade.SpriteList()
+    room.coin_list = arcade.SpriteList()
+    room.star_list = arcade.SpriteList()
+    room.enemy_list = arcade.SpriteList()
 
     # Walls, Room 3, image from https://kenney.nl
     for y in (0, SCREEN_HEIGHT - 16):
@@ -211,6 +233,58 @@ def setup_room_3():
         wall.center_y = y
         room.wall_list.append(wall)
 
+    # Flowers, image from https://kenney.nl
+    for i in range(NUMBER_OF_COINS):
+        coin = arcade.Sprite("foliagePack_001.png", SPRITE_SCALING)
+
+        # Boolean variable coin placement
+        coin_placed_successfully = False
+
+        while not coin_placed_successfully:
+            # Position the coin
+            coin.center_x = random.randrange(SCREEN_WIDTH - 25)
+            coin.center_y = random.randrange(SCREEN_HEIGHT - 25)
+
+            # See if the coin is hitting a wall
+            wall_hit_list = arcade.check_for_collision_with_list(coin, room.wall_list)
+            # See if the coin is hitting another coin
+            coin_hit_list = arcade.check_for_collision_with_list(coin, room.coin_list)
+            if len(wall_hit_list) == 0 and len(coin_hit_list) == 0:
+                # It is!
+                coin_placed_successfully = True
+        # Add the coin to the lists
+        room.coin_list.append(coin)
+
+    # Mushroom, image from https://kenney.nl
+    for i in range(NUMBER_OF_STARS):
+        star = arcade.Sprite("mushroomRed.png", SPRITE_SCALING)
+
+        # Boolean variable star placement
+        star_placed_successfully = False
+
+        while not star_placed_successfully:
+            # Position the stars
+            star.center_x = random.randrange(SCREEN_WIDTH - 25)
+            star.center_y = random.randrange(SCREEN_HEIGHT - 25)
+
+            # See if the star is hitting a wall
+            wall_hit_list = arcade.check_for_collision_with_list(star, room.wall_list)
+            # See if the star is hitting another star or coin
+            star_hit_list = arcade.check_for_collision_with_list(coin, room.coin_list, room.star_list)
+            if len(wall_hit_list) == 0 and len(coin_hit_list) == 0:
+                # It is!
+                star_placed_successfully = True
+        # Add the star to the lists
+        room.star_list.append(star)
+
+        # Bee, image from https://kenney.nl
+        for i in range(ENEMY_NUMBER):
+
+            enemy = Enemy("bee.png", SPRITE_SCALING)
+            enemy.center_x = random.randrange(SCREEN_WIDTH - 25)
+            enemy.center_y = random.randrange(SCREEN_HEIGHT - 25)
+            room.enemy_list.append(enemy)
+
     return room
 
 
@@ -220,6 +294,8 @@ def setup_room_4():
 
     # Sprite lists
     room.wall_list = arcade.SpriteList()
+    room.coin_list = arcade.SpriteList()
+    room.star_list = arcade.SpriteList()
 
     # Walls, Room 4, image from https://kenney.nl
     for x in range(50, SCREEN_WIDTH, 63):
@@ -267,6 +343,8 @@ def setup_room_5():
 
     # Sprite lists
     room.wall_list = arcade.SpriteList()
+    room.coin_list = arcade.SpriteList()
+    room.star_list = arcade.SpriteList()
 
     # Walls, Room 5, image from https://kenney.nl
     for x in range(50, SCREEN_WIDTH, 63):
@@ -323,12 +401,13 @@ class MyGame(arcade.Window):
         # Set up the player
         self.rooms = None
         self.player_sprite = None
-        # Sound
-        self.laser_sound = arcade.load_sound("laser.wav")
         self.player_list = None
         self.coin_list = None
         self.star_list = None
+        self.enemy_list = None
         self.physics_engine = None
+        # Sound
+        self.laser_sound = arcade.load_sound("laser.wav")
 
     def setup(self):
         """ Set up the game and initialize the variables. """
@@ -380,6 +459,10 @@ class MyGame(arcade.Window):
         # Draw stars
         self.rooms[self.current_room].star_list.draw()
 
+        # Draw enemy
+        self.rooms[self.current_room].enemy_list.draw()
+
+        # Draw player
         self.player_list.draw()
         arcade.draw_text(f"Score: {self.score}", 10, 10, arcade.color.WHITE, 24)
 
@@ -410,6 +493,8 @@ class MyGame(arcade.Window):
                                                               self.rooms[self.current_room].coin_list)
         star_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
                                                              self.rooms[self.current_room].star_list)
+        enemy_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                              self.rooms[self.current_room].enemy_list)
 
         # Loop through each colliding sprite, remove it, and add to the score.
         for coin in coins_hit_list:
@@ -420,6 +505,13 @@ class MyGame(arcade.Window):
             arcade.play_sound(self.laser_sound)
             star.remove_from_sprite_lists()
             self.score += 5
+        for enemy in enemy_hit_list:
+            enemy.follow_sprite(self.player_sprite)
+            enemy.remove_from_sprite_lists()
+            self.score -= 3
+
+        # If score drops below 0, game ends
+        #if self.score < 0:
 
         # Room logic
         if self.player_sprite.center_x > SCREEN_WIDTH and self.current_room == 0:
