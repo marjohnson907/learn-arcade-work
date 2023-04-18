52cd686e9d0295419e488093d09b76a740c09153
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2023-04-17 18:26:59 -0600

    Added coins and sound to room 2

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -5,6 +5,7 @@ SPRITE_SCALING = 0.5
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
 NUMBER_OF_COINS = 10
+NUMBER_OF_STARS = 5
 MOVEMENT_SPEED = 5
 
 
@@ -14,6 +15,7 @@ class Room:
         # Lists
         self.wall_list = None
         self.coin_list = None
+        self.star_list = None
 
 
 def setup_room_1():
@@ -23,6 +25,7 @@ def setup_room_1():
     # Sprite lists
     room.wall_list = arcade.SpriteList()
     room.coin_list = arcade.SpriteList()
+    room.star_list = arcade.SpriteList()
 
     # Walls, Room 1, image from https://kenney.nl
     for y in (0, SCREEN_HEIGHT - 16):
@@ -87,6 +90,8 @@ def setup_room_2():
 
     # Sprite lists
     room.wall_list = arcade.SpriteList()
+    room.coin_list = arcade.SpriteList()
+    room.star_list = arcade.SpriteList()
 
     # Walls, Room 2, image from https://kenney.nl
     for y in (0, SCREEN_HEIGHT - 16):
@@ -119,6 +124,50 @@ def setup_room_2():
         wall.center_x = SCREEN_WIDTH
         wall.center_y = y
         room.wall_list.append(wall)
+
+    # Coins, image from https://kenney.nl
+    for i in range(NUMBER_OF_COINS):
+        coin = arcade.Sprite("coinSilver.png", SPRITE_SCALING / 2)
+
+        # Boolean variable coin placement
+        coin_placed_successfully = False
+
+        while not coin_placed_successfully:
+            # Position the coin
+            coin.center_x = random.randrange(SCREEN_WIDTH - 25)
+            coin.center_y = random.randrange(SCREEN_HEIGHT - 25)
+
+            # See if the coin is hitting a wall
+            wall_hit_list = arcade.check_for_collision_with_list(coin, room.wall_list)
+            # See if the coin is hitting another coin
+            coin_hit_list = arcade.check_for_collision_with_list(coin, room.coin_list)
+            if len(wall_hit_list) == 0 and len(coin_hit_list) == 0:
+                # It is!
+                coin_placed_successfully = True
+        # Add the coin to the lists
+        room.coin_list.append(coin)
+
+        # Stars, image from https://kenney.nl
+        for i in range(NUMBER_OF_STARS):
+            star = arcade.Sprite("star.png", SPRITE_SCALING / 2)
+
+            # Boolean variable star placement
+            star_placed_successfully = False
+
+            while not star_placed_successfully:
+                # Position the stars
+                star.center_x = random.randrange(SCREEN_WIDTH - 25)
+                star.center_y = random.randrange(SCREEN_HEIGHT - 25)
+
+                # See if the star is hitting a wall
+                wall_hit_list = arcade.check_for_collision_with_list(star, room.wall_list)
+                # See if the star is hitting another star or coin
+                star_hit_list = arcade.check_for_collision_with_list(coin, room.coin_list, room.star_list)
+                if len(wall_hit_list) == 0 and len(coin_hit_list) == 0:
+                    # It is!
+                    star_placed_successfully = True
+            # Add the star to the lists
+            room.star_list.append(star)
 
     return room
 
@@ -211,6 +260,7 @@ def setup_room_4():
 
     return room
 
+
 def setup_room_5():
     """Room 5"""
     room = Room()
@@ -277,6 +327,7 @@ class MyGame(arcade.Window):
         self.laser_sound = arcade.load_sound("laser.wav")
         self.player_list = None
         self.coin_list = None
+        self.star_list = None
         self.physics_engine = None
 
     def setup(self):
@@ -326,6 +377,9 @@ class MyGame(arcade.Window):
         # Draw coins
         self.rooms[self.current_room].coin_list.draw()
 
+        # Draw stars
+        self.rooms[self.current_room].star_list.draw()
+
         self.player_list.draw()
         arcade.draw_text(f"Score: {self.score}", 10, 10, arcade.color.WHITE, 24)
 
@@ -351,17 +405,21 @@ class MyGame(arcade.Window):
         """ Movement and game logic """
         self.physics_engine.update()
 
-        # self.coin_list.update()
-
         # Generate a list of all sprites that collided with the player.
         coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
                                                               self.rooms[self.current_room].coin_list)
+        star_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                             self.rooms[self.current_room].star_list)
 
         # Loop through each colliding sprite, remove it, and add to the score.
         for coin in coins_hit_list:
             arcade.play_sound(self.laser_sound)
             coin.remove_from_sprite_lists()
             self.score += 1
+        for star in star_hit_list:
+            arcade.play_sound(self.laser_sound)
+            star.remove_from_sprite_lists()
+            self.score += 5
 
         # Room logic
         if self.player_sprite.center_x > SCREEN_WIDTH and self.current_room == 0:
